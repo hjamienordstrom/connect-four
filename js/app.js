66635cc09143a3f17aa26ddf1c404e502a4e1b15
Hanna Nordstrom <hjamienordstrom@Hannas-MacBook-Pro.local>
2021-06-01 16:12:50 -0400

    rip

---
+++ js/app.js
@@ -0,0 +1,19 @@
+//random path is generated from bottom to top
+
+//player may only move one space at a time
+
+//can only go to square 'touching' it
+
+//can only go forward/diagnal forward
+
+//path is hidden-meaning it doesnt show until square is chosen
+
+//tile turns red or green depending on right choice
+
+//if incorrect, game restarts
+
+//player must memorize path
+
+//once player gets to the other side, congratulations is presented\
+
+console.log('hope this works')
